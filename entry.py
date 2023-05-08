eaa6de702cf6edc9d172f184ff5baf838ac3c8c0
Wovi10 <wout.vinck@gmail.com>
2023-05-08 16:40:26 +0200

    Update entry.py

--- entry.py
+++ entry.py
@@ -1,12 +1,12 @@
 import sys
-from myModules import ticketManager
+from my_modules import ticket_manager
 
 def main(arg):
-    ticketManager.manage(arg)
+    ticket_manager.manage(arg)
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Error: A ticketName is required.")
         sys.exit(1)
-    param = sys.argv[1]
-    main(param)
+    PARAM = sys.argv[1]
+    main(PARAM)
