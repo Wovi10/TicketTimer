684560747b33f9ed97b26f5b3210e0f82138bd16
Wovi10 <wout.vinck@gmail.com>
2023-06-19 16:14:10 +0200

    Update delete.py

--- delete.py
+++ delete.py
@@ -6,9 +6,7 @@ from my_modules import ticket_manager, logger
 from my_modules.input_sanitiser import sanitise_script_input
 
 def main(arg):
-    logger.log()
     ticket_manager.delete_entry(arg)
-    logger.log()
 
 if __name__ == "__main__":
     sanitise_script_input()
